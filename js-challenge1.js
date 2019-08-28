8add364f82b28606e660c02fa45633274324fc71
mNamuli <mnamuli81@gmail.com>
2019-08-28 12:00:13 +0300

    Challenge 1 input/output/variables/conditional Logic

---
+++ js-challenge1.js
@@ -0,0 +1,13 @@
+
+function myFirst() { 
+    var year = window.prompt("Enter your Birth Year: ");
+    output = 2019-year;
+
+    if (output<18) {
+        alert("Your're a MINOR");
+      } else if (output<36) {
+        alert("Your're a YOUTH");
+      } else{
+        alert("Your're an ELDER");
+      }
+} 
