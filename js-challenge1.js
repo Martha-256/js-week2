368e6a4ff102454340e1df2e8fc9579c7686ba88
mNamuli <mnamuli81@gmail.com>
2019-08-31 08:08:02 +0300

    running in Terminal

--- js-challenge1.js
+++ js-challenge1.js
@@ -1,13 +1,17 @@
 
-function myFirst() { 
-    var year = window.prompt("Enter your Birth Year: ");
-    output = 2019-year;
-
-    if (output<18) {
-        alert("Your're a MINOR");
-      } else if (output<36) {
-        alert("Your're a YOUTH");
-      } else{
-        alert("Your're an ELDER");
+function myfunction(){
+  console.log("Enter your year of Birth");
+  process.stdin.once('data', (number)=>{
+  
+      var age=2019-number;
+      if (age<18){
+          console.log("You're a MINOR!");
+      }else if(age<36){
+          console.log("You're a YOUTH");
+      }else{
+          console.log("You're an ELDER!");
       }
-} 
+   
+  })
+  }
+  console.log(myfunction());
